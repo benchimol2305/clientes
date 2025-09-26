8e5a0c202dda1ac11dd167e06a16c0a22b8bc923
benchimol2305 <cesarbenchimoldavila@gmail.com>
2025-09-26 00:00:45 -0400

    agregar mas mensajes al programa

--- git.cpp
+++ git.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
-    cout << "Mi nombre es: Cesar" << endl;
+    cout << "Mi nombre es: Cesar" << endl;;
+    cout << "Estoy aprendiendo Git en Programación II" << endl;
+    cout << "Este es mi segundo commit!" << endl;
     return 0;
 }
