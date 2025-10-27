96e5a78069531110a6cf47a62af246436e9fe378
benchimol2305 <cesarbenchimoldavila@gmail.com>
2025-10-27 00:15:56 -0400

    creo una funcion que cargue una matriz de ejemplo y una funcion que actualice las matrices desde los inputs

--- matrices.js
+++ matrices.js
@@ -11,8 +11,8 @@ function initializeMatrices() {
     tipo_de_matriz = parseInt(document.getElementById('tipo_de_matriz').value);
     createMatrixInputs('matriz_a', tipo_de_matriz);
     createMatrixInputs('matriz_b', tipo_de_matriz);
-    matrizA = createEmptyMatrix(tipo_de_matriz);
-    matrizB = createEmptyMatrix(tipo_de_matriz);
+    matrizA = createEmptyMatriz(tipo_de_matriz);
+    matrizB = createEmptyMatriz(tipo_de_matriz);
 }
 
 function createMatrixInputs(containerId, size) {
@@ -80,8 +80,47 @@ function generateRandomMatrix() {
 }
 // para limpiar la matriz
 function clearMatrices() {
-    matrizA = createEmptyMatrix(tipo_de_matriz);
-    matrizB = createEmptyMatrix(tipo_de_matriz);
+    matrizA = createEmptyMatriz(tipo_de_matriz);
+    matrizB = createEmptyMatriz(tipo_de_matriz);
     updateMatrixInputs();
     clearResult();
+}
+
+function loadExampleMatrix() {
+    tipo_de_matriz = 3;
+    document.getElementById('tipo_de_matriz').value = tipo_de_matriz;
+    matrizA = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
+    matrizB = [
+        [9, 8, 7],
+        [6, 5, 4],
+        [3, 2, 1]
+    ];
+
+    createMatrixInputs('matriz_a', tipo_de_matriz);
+    createMatrixInputs('matriz_b', tipo_de_matriz);
+    updateMatrixInputs();
+}
+
+function updateMatricesFromInputs() {
+    const inputsA = document.querySelectorAll('#matriz_a input');
+    const inputsB = document.querySelectorAll('#matriz_b input');
+
+    matrizA = createEmptyMatriz(tipo_de_matriz);
+    matrizB = createEmptyMatriz(tipo_de_matriz);
+    
+    inputsA.forEach(input => {
+        const row = parseInt(input.dataset.row);
+        const col = parseInt(input.dataset.col);
+        matrizA[row][col] = parseFloat(input.value) || 0;
+    });
+    
+    inputsB.forEach(input => {
+        const row = parseInt(input.dataset.row);
+        const col = parseInt(input.dataset.col);
+        matrizB[row][col] = parseFloat(input.value) || 0;
+    });
 }
