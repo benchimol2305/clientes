6f039e96a21bedad089ad9ceea18c0d87140137f
benchimol2305 <cesarbenchimoldavila@gmail.com>
2025-11-20 01:32:02 -0400

    se declaran las variables que se usaran para este proyecto

---
+++ script.js
@@ -0,0 +1,13 @@
+// variables del juego
+let playerName = '';
+let questionCount = 10;
+let difficulty = '';
+let category = '';
+let questions = [];
+let currentQuestionIndex = 0;
+let score = 0;
+let correctAnswers = 0;
+let timer;
+let timeLeft = 20;
+let totalTimeSpent = 0;
+let categories = [];
